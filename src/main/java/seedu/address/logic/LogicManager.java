13f8a208501443986ec38b75af95206080faf180
huekoh <huexkoh@gmail.com>
2024-04-14 11:36:06 +0800

    Fix on-launch viewable list
    On launch, all contacts, archived and unarchived were listed. Fix to only show unarchived contacts.

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -86,6 +86,11 @@ public class LogicManager implements Logic {
     }
 
     @Override
+    public ObservableList<Person> getFilteredUnarchivedPersonList() {
+        return model.getFilteredUnarchivedPersonList();
+    }
+
+    @Override
     public Path getAddressBookFilePath() {
         return model.getAddressBookFilePath();
     }
